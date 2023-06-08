38b19399f64586941711582011113314b3c140d4
lucas <lucaslima.94@hotmail.com.br>
2023-06-08 08:20:01 -0300

    add fonts and custom home page

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_gpt_app/widgets/fixed_spacer.dart';
+import '../widgets/wave_shape.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,40 +13,56 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(18),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FixedSpacer.vBiggest,
-              FixedSpacer.vBig,
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.asset(
-                  "assets/images/luffy_profile.png",
-                  height: 250,
-                  width: 250,
-                ),
-              ),
-              FixedSpacer.vBiggest,
-              const SizedBox(
-                child: Text("Bem Vind@ ao Quizz de One Piece"),
-              ),
-              FixedSpacer.vBiggest,
-              const SizedBox(
-                width: 400,
-                child: Text(
-                  "Bem-vind@ ao nosso aplicativo de perguntas e respostas sobre One Piece! \n Prepare-se para mergulhar no incrível mundo dos piratas, aventuras emocionantes e mistérios cativantes. \nEstamos aqui para ajudá-lo a explorar e aprofundar seu conhecimento sobre esse anime épico.\n Divirta-se e embarque nessa jornada inesquecível!",
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              FixedSpacer.vBiggest,
-              ElevatedButton(
-                  onPressed: () {}, child: const Text("Vamos começar!"))
-            ],
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.blue[200],
           ),
-        ),
+          CustomPaint(
+            painter: WavePainter(),
+            child: Padding(
+              padding: const EdgeInsets.all(18),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FixedSpacer.vBiggest,
+                    FixedSpacer.vBig,
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(60.0),
+                      child: Image.asset(
+                        "assets/images/luffy_profile.png",
+                        height: 250,
+                        width: 250,
+                      ),
+                    ),
+                    FixedSpacer.vBiggest,
+                    const SizedBox(
+                      child: Text(
+                        "Bem-Vind@ ao Quizz de One Piece",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'OnePiece',
+                            fontSize: 28),
+                      ),
+                    ),
+                    FixedSpacer.vBiggest,
+                    const SizedBox(
+                      width: 400,
+                      child: Text(
+                        "Bem-vind@ ao nosso aplicativo de perguntas e respostas sobre One Piece! \n Prepare-se para mergulhar no incrível mundo dos piratas, aventuras emocionantes e mistérios cativantes. \nEstamos aqui para ajudá-lo a explorar e aprofundar seu conhecimento sobre esse anime épico.\n Divirta-se e embarque nessa jornada inesquecível!",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    FixedSpacer.vBiggest,
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text("Vamos começar!"))
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
